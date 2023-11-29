06d46711a2874c2410cfae1d75948cfacd7f54ab
AGaleea <a_galeeva@hotmail.com>
2023-11-29 18:25:08 +0100

    homework20 added

---
+++ src/homeworks/homework20/Homework20.tsx
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+import './styles.css';
+
+function Homework20() {
+  let firstName: string = "John";
+  let lastName: string = "Doe";
+  let job: string = "Software Engineer";
+  let isManager: boolean = false;
+  let experience: number = 5;
+  let email: string = "john.doe@google.com";
+  let state: string = !isManager ? 'Employee' : 'Manager'   //  'Manager' === 'Employee'   :)))
+
+  let programLangs: string[] = ['JavaScript', 'Python', 'Scala'] // 'Machine Learning' === 'Python' :)))
+
+  const showProgramLangs = programLangs.map((el) => {
+    return (
+      <li>
+        {el}
+      </li>
+      );
+  });
+  
+  return <div className='hw20-wrapper'>
+    <div className='card-container'>
+      <p>First name: {firstName}</p>
+      <p>Last name: {lastName}</p>
+      <p>Occupation: {job}</p>
+      <p>Position: {state}</p>
+      <p>Experience: {experience} years</p>
+      <p>E-mail: {email}</p>
+    </div>
+    <div  className='card-container'>
+      <ul>{showProgramLangs}</ul>
+    </div>
+  </div>;
+}
+
+export default Homework20;
