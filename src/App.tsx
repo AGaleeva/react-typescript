eaefa3e1af52de35c6b2be74a4efdc0c68bf6289
AGaleea <a_galeeva@hotmail.com>
2023-12-04 13:15:00 +0100

    lesson22 added

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,17 @@ import Lesson20 from './lessons/lesson20';
 import Homework20 from './homeworks/homework20';
 import Lesson21 from './lessons/lesson21';
 import Homework21 from './homeworks/homework21';
+import Lesson22 from 'lessons/lesson22';
 
 function App() {
   return (
     <div className="App">      
       {/* <Lesson20 />  */}
       {/* <Lesson21 /> */}
+      <Lesson22 />
 
       {/* <Homework20 /> */}
-      <Homework21 />
+      {/* <Homework21 /> */}
     </div>
   );
 }
