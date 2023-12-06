6cfc9658b3c2171a9cc32b6dc11abdbc7aca05d4
AGaleea <a_galeeva@hotmail.com>
2023-12-06 11:32:14 +0100

    hw23 fixed

--- src/homeworks/homework23/Homework23.tsx
+++ src/homeworks/homework23/Homework23.tsx
@@ -1,85 +1,144 @@
-import { useState, ChangeEvent } from 'react';
+import { useState, ChangeEvent, SetStateAction, Dispatch } from "react";
 import Input from "components/input";
-import { Homework23Wrapper, LoginCard } from "./styles";
-import Button from 'components/button';
+import {
+  Homework23Wrapper,
+  LoginCard,
+  PersonCard,
+  PersonInfoBig,
+  PersonInfoContainer,
+  PersonInfoSmall,
+} from "./styles";
+import Button from "components/button";
+
+interface UserInfo {
+  name: string;
+  surname: string;
+  age: string;
+  position: string;
+}
 
 function Homework23() {
-  const [nameValue, setNameValue] = useState<string>('');
-  const [surnameValue, setSurnameValue] = useState<string>('');
-  const [ageValue, setAgeValue] = useState<string>('');
-  const [positionValue, setPositionValue] = useState<string>('');
+  const [nameValue, setNameValue] = useState<string>("");
+  const [surnameValue, setSurnameValue] = useState<string>("");
+  const [ageValue, setAgeValue] = useState<string>("");
+  const [positionValue, setPositionValue] = useState<string>("");
 
-  const onChangeNameValue = (event: ChangeEvent<HTMLInputElement>): void => {
-    setNameValue(event.target.value);
-    // console.log(event.target.value);
-  }
-  const onChangeSurnameValue = (event: ChangeEvent<HTMLInputElement>): void => {
-    setSurnameValue(event.target.value);
-    // console.log(event.target.value);
-  }
-  const onChangeAgeValue = (event: ChangeEvent<HTMLInputElement>): void => {
-    setAgeValue(event.target.value);
-    // console.log(event.target.value);
-  }
-  const onChangePositionValue = (event: ChangeEvent<HTMLInputElement>): void => {
-    setPositionValue(event.target.value);
-    // console.log(event.target.value);
-  }
-      
-  return (  
+  // создать state, который решает когда показывать карточку, а когда нет
+  const [isShowCard, setIsShowCars] = useState<boolean>(false);
+
+  // создаем контейнер (state), в котором будет храниться инф-я для карточки, чтобы она туда добавлялась только на onClick
+  const [userInfo, setUserInfo] = useState<UserInfo>({name: '', surname: '', age: '', position: ''});
+
+
+  // const onChangeNameValue = (event: ChangeEvent<HTMLInputElement>): void => {
+  //   setNameValue(event.target.value);    
+  // };
+  // const onChangeSurnameValue = (event: ChangeEvent<HTMLInputElement>): void => {
+  //   setSurnameValue(event.target.value);   
+  // };
+  // const onChangeAgeValue = (event: ChangeEvent<HTMLInputElement>): void => {
+  //   setAgeValue(event.target.value);    
+  // };
+  // const onChangePositionValue = (
+  //   event: ChangeEvent<HTMLInputElement>
+  // ): void => {
+  //   setPositionValue(event.target.value);    
+  // };
+  const onChangeInputValue = (
+    event: ChangeEvent<HTMLInputElement>,
+    setInputValue: Dispatch<SetStateAction<string>>
+  ) => {
+    setInputValue(event.target.value);
+  };
+
+  return (
     <Homework23Wrapper>
       <LoginCard>
-        <Input 
-          placeholder='First name'
-          label='First name'
-          name='firstName'
-          type='text'
+        <Input
+          placeholder="First name"
+          label="First name"
+          name="firstName"
+          type="text"
           value={nameValue}
-          onChange={onChangeNameValue}
-        />        
-        <Input 
-          placeholder='Last name'
-          label='Last name'
-          name='lastName'
-          type='text'
-          value={surnameValue}
-          onChange={onChangeSurnameValue}
-        />
-        <Input 
-          placeholder='Age'
-          label='Age'
-          name='age'
-          type='text'
-          value={ageValue}
-          onChange={onChangeAgeValue}
-        />
-        <Input 
-          placeholder='Position'
-          label='Position'
-          name='position'
-          type='text'
-          value={positionValue}
-          onChange={onChangePositionValue}
-        />
-        <Button 
-          name={'Create personal card'} 
-          // type='submit'
-          onClick={() => {
-            return (
-              <LoginCard>
-                <p>First name: {nameValue}</p>
-                <p>Last name: {surnameValue}</p>
-                <p>Age: {ageValue}</p>
-                <p>Position: {positionValue}</p>
-              </LoginCard>
-            );            
-            } 
+          onChange={(event: ChangeEvent<HTMLInputElement>) =>
+            onChangeInputValue(event, setNameValue)
           }
         />
-      </LoginCard> 
-  
+        <Input
+          placeholder="Last name"
+          label="Last name"
+          name="lastName"
+          type="text"
+          value={surnameValue}
+          onChange={(event: ChangeEvent<HTMLInputElement>) =>
+            onChangeInputValue(event, setSurnameValue)
+          }
+        />
+        <Input
+          placeholder="Age"
+          label="Age"
+          name="age"
+          type="text"
+          value={ageValue}
+          onChange={(event: ChangeEvent<HTMLInputElement>) =>
+            onChangeInputValue(event, setAgeValue)
+          }
+        />
+        <Input
+          placeholder="Position"
+          label="Position"
+          name="position"
+          type="text"
+          value={positionValue}
+          onChange={(event: ChangeEvent<HTMLInputElement>) =>
+            onChangeInputValue(event, setPositionValue)
+          }
+        />
+        <Button
+          name={"Create person card"}
+          // type='submit'
+          onClick={() => {
+            if (
+              !!nameValue &&
+              !!surnameValue &&
+              !!ageValue &&
+              !!positionValue
+            ) {
+              setUserInfo({
+                name: nameValue,
+                surname: surnameValue,
+                age: ageValue,
+                position: positionValue,
+              });
+              setIsShowCars(true);
+            } else {
+              setIsShowCars(false);
+              setTimeout(() => alert("Please enter all data"), 5);
+            }
+          }}
+        />
+      </LoginCard>
+      {isShowCard && (
+        <PersonCard>
+          <PersonInfoContainer>
+            <PersonInfoSmall>First name</PersonInfoSmall>
+            <PersonInfoBig>{userInfo.name}</PersonInfoBig>
+          </PersonInfoContainer>
+          <PersonInfoContainer>
+            <PersonInfoSmall>Last name</PersonInfoSmall>
+            <PersonInfoBig>{userInfo.surname}</PersonInfoBig>
+          </PersonInfoContainer>
+          <PersonInfoContainer>
+            <PersonInfoSmall>Age</PersonInfoSmall>
+            <PersonInfoBig>{userInfo.age}</PersonInfoBig>
+          </PersonInfoContainer>
+          <PersonInfoContainer>
+            <PersonInfoSmall>Position</PersonInfoSmall>
+            <PersonInfoBig>{userInfo.position}</PersonInfoBig>
+          </PersonInfoContainer>
+        </PersonCard>
+      )}
     </Homework23Wrapper>
   );
 }
-
 export default Homework23;
--- src/homeworks/homework23/styles.ts
+++ src/homeworks/homework23/styles.ts
@@ -4,10 +4,10 @@ import { colors } from "styles/colors";
 
 export const Homework23Wrapper = styled.div`
   display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: space-evenly;
-  padding: 100px;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 400px;
   width: 100%;
   height: 100%;
   background-color: ${colors.primary};
@@ -26,4 +26,36 @@ export const LoginCard = styled.form`
   min-width: 470px;
   border-radius: 4px;
   background-color: #fff;
+`;
+
+export const PersonCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-width: 400px;
+  min-height: 400px;
+  height: fit-content;
+  width: fit-content;
+  border-radius: 4px;
+  padding: 60px;
+  gap: 30px;
+  background-color: white;
+`;
+
+export const PersonInfoSmall = styled.p`
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 14px;
+  /* font-size: 16px; */
+  color: grey;
+`;
+
+export const PersonInfoBig = styled.p`
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 28px;
+  font-weight: 500;
+`;
+
+export const PersonInfoContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 6px;
 `;
